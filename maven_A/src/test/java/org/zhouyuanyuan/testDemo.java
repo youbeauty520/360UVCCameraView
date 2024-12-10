014c913aa361eecc0015b0b838caa7070f5d597d
youbeauty520 <youbeauty520@126.com>
2024-12-10 17:52:15 +0800

    java项目

---
+++ maven_A/src/test/java/org/zhouyuanyuan/testDemo.java
@@ -0,0 +1,10 @@
+package org.zhouyuanyuan;
+
+import org.junit.Test;
+
+public class testDemo {
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+}
---
+++ maven_B/src/main/java/org/zhouyuanyuan/Main.java
@@ -0,0 +1,7 @@
+package org.zhouyuanyuan;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
