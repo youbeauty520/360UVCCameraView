0788efeafae11099951ceb3d04bd8259418e4ef6
youbeauty520 <youbeauty520@126.com>
2025-01-10 15:35:57 +0800

    1

--- spring_first/src/main/java/org/zhouyuanyuan/spring_first/Service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package org.zhouyuanyuan.spring_first.Service;
-
-import org.zhouyuanyuan.spring_first.prjo.Person;
-
-import java.util.List;
-//获取所有的员工
-public interface PersonService {
-    List<Person> findAllPersons();
-}
